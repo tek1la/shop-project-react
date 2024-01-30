cb1d707db25894c64c5755420c530954c4950fea
Vitalii <v.surkov365@gmail.com>
2024-01-30 22:28:39 +0200

    style

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,14 +6,14 @@ type HeaderType = {
 }
 
 const Header = (props: HeaderType) => {
+    const style = {
+        backgroundColor: props.bgColor,
+        color: props.position ? 'white' : 'black',
+        padding: '15px',
+    }
+
     return (
-        <h1
-            style={{
-                backgroundColor: props.bgColor,
-                color: props.position ? 'white' : 'black',
-                padding: '15px',
-            }}
-        >
+        <h1 style={style}>
             Hello world, {props.title} {props.num}
         </h1>
     )
