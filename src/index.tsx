bfa8de6eb221d5023edab2fe5f2641336993d65f
Vitalii <v.surkov365@gmail.com>
2024-02-01 19:11:01 +0200

    added content component

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header'
+import Content from './Content'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// whithout jsx
-// const el = React.createElement('h1', { id: 'title' }, 'Hello title!')
-
-// whith jsx
-// let a = 10
-// const el = <h1 id="title">Hello title! {a}</h1>
-
-const el2 = (
-    <div>
-        <h1>Hello Title!</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-            nesciunt, culpa eaque voluptatibus animi assumenda! Earum iusto
-            corporis delectus, quod porro perspiciatis atque nostrum deleniti
-            expedita sequi sed dolorem excepturi!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-            accusantium id totam praesentium deleniti voluptate rerum
-            consequatur aliquam, molestiae corrupti illum dicta adipisci quia
-            aspernatur distinctio architecto omnis. Dicta, vel!
-        </p>
-    </div>
-)
-
-function Content() {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-                odio repellendus, beatae laudantium ratione earum eos molestiae
-                pariatur vitae tempore. Mollitia maiores tempora nostrum minima
-                beatae distinctio iste aliquid impedit.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-                odio repellendus, beatae laudantium ratione earum eos molestiae
-                pariatur vitae tempore. Mollitia maiores tempora nostrum minima
-                beatae distinctio iste aliquid impedit.
-            </p>
-        </React.Fragment>
-    )
-}
 
 const App = () => {
     return (
         <>
             <Header title="App.js" num={10} />
             <Header title="React.js" />
-            <Content />
+            <Content
+                content="1. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quod illum accusamus magnam veritatis nobis rem aliquid excepturi quis soluta, 
+            natus ad nulla assumenda autem labore velit maiores. Dolorum, voluptatum assumenda?"
+                content2="Luck"
+            />
+            <Content
+                content="2. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quod illum accusamus magnam veritatis nobis rem aliquid excepturi quis soluta, 
+            natus ad nulla assumenda autem labore velit maiores. Dolorum, voluptatum assumenda?"
+                content2="No luck"
+                bgRed={true}
+            />
         </>
     )
 }
+
 root.render(
     <React.StrictMode>
-        {el2}
         <App />
     </React.StrictMode>
 )
