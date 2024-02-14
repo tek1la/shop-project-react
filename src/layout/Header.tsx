81c0484d9536a981fcb2c6af4297b5f2176e1b33
Vitalii <v.surkov365@gmail.com>
2024-02-14 10:49:50 +0200

    added Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,8 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
-import { green } from '@mui/material/colors'
+import Logo from 'components/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -18,22 +15,7 @@ const Header = (props: Props) => {
         >
             <Container maxWidth="lg">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
